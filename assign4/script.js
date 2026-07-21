3ddaed8718734ef3dcc81e53387cf368269e5d15
Joel ngniayie <joelstephane.ngniayiedomtchoua@dcmail.ca>
2026-07-21 11:32:37 -0400

    generateStory()

--- assign4/script.js
+++ assign4/script.js
@@ -9,43 +9,55 @@ function randomValueFromArray(array) {
   return array[random];
 }
 
-// Raw text strings
+// Raw text string arrays
 
-// Willy the Goblin
-// Big Daddy
-// Father Christmas
+const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const insertY = ["the soup kitchen", "Disneyland", "the White House"];
+const insertZ = [
+  "spontaneously combusted",
+  "melted into a puddle on the sidewalk",
+  "turned into a slug and slithered away",
+];
 
-// the soup kitchen
-// Disneyland
-// the White House
-
-// spontaneously combusted
-// melted into a puddle on the sidewalk
-// turned into a slug and slithered away
-
-// Partial return random string function
+// Return random story string function
 
 function returnRandomStoryString() {
-  // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+  const storyText =
+    "It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
-  return storyText;
+  let newStory = storyText;
+
+  const xItem = randomValueFromArray(insertX);
+  const yItem = randomValueFromArray(insertY);
+  const zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replace(/:insertx:/g, xItem);
+  newStory = newStory.replace(":inserty:", yItem);
+  newStory = newStory.replace(":insertz:", zItem);
+
+  return newStory;
 }
 
-// Event listener and partial generate function definition
+// Event listener and generate function definition
 
 generateBtn.addEventListener("click", generateStory);
 
 function generateStory() {
+  const newStory = returnRandomStoryString();
+  let finalStory = newStory;
+
   if (customName.value !== "") {
     const name = customName.value;
+    finalStory = finalStory.replace(/Bob/g, name);
   }
 
   if (document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature = Math.round(94);
+    const weight = Math.round(300 / 14); // pounds -> stone
+    const temperature = Math.round((94 - 32) * (5 / 9)); // Fahrenheit -> Celsius
+    finalStory = finalStory.replace("94 Fahrenheit", `${temperature} Celsius`);
+    finalStory = finalStory.replace("300 pounds", `${weight} stone`);
   }
 
-  // TODO: replace "" with the correct expression
-  story.textContent = "";
+  story.textContent = finalStory;
   story.style.visibility = "visible";
 }
